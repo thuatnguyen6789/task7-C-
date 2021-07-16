efd5c5961b06a05aa43d98d0ff688bccec83c6bd
Thuat Nguyen <73330731+thuatnguyen6789@users.noreply.github.com>
2021-07-16 09:20:46 +0700

    Add files via upload

---
+++ Session8/Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+interface IMath
+{
+    void Area();
+}
+class Circle : IMath
+{
+    public const float PI = 3.14f;
+    protected float Radius;
+    protected double AreaOfCircle;
+    public virtual void Area()
+    {
+        AreaOfCircle = (PI * Radius * Radius);
+    }
+}
+class Sphere : Circle
+{
+    double areaOfSphere;
+    public override void Area()
+    {
+        base.Area();
+        areaOfSphere = (AreaOfCircle * 4);
+    }
+    static void Main(string[] args)
+    {
+        Sphere objSphere = new Sphere();
+        objSphere.Radius = 7;
+        objSphere.Area();
+        Console.WriteLine("Area of Sphere:{0:F2}", objSphere.areaOfSphere);
+    }
+}
---
+++ Session8/Program4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface ISet
+{
+    void Accept Details(int valOne, string valTwo);
+
+}
+interface IGet
+{
+    void Display();
+}
+
+class Employee : ISet
+{
+    int_empID;
+        string_empName;
+        public void Accept Details (int valOne, string valTwo)
+    {
+        _empID = valOne;
+        _empName = valTwo;
+    }
+    static void Main (string[]args)
+    {
+        Employee objEmployee = new Employee();
+        objEmployee.Accept Details(10, "Jack");
+        IGet objGet = objEmployee as IGet;
+        if (objGet != null)
+        {
+            objGet.Display();
+        }
+        else
+        {
+            Console.WriteLine("Invalid casting occurred");
+        }
+    }
+}
